efae5aab578603bfe98ba411b13e8008ce41db41
mrx7355608 <mrx7355608@gmail.com>
2024-09-01 18:52:46 +0500

    added color mode script

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react';
+import React from "react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "@/theme";
 
-export default function Provider({children}: {children: React.ReactNode}) {
-    return <ChakraProvider>{children}</ChakraProvider>
+export default function Provider({ children }: { children: React.ReactNode }) {
+    return (
+        <ChakraProvider>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            {children}
+        </ChakraProvider>
+    );
 }
